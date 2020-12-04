5b08d18ee718318c6ffec1b05a20e06d75d138f7
dishant2000 <kumawatdishant90@gmail.com>
2020-12-04 12:14:36 +0530

    Updates

---
+++ precache-manifest.8e71c2099ccf5b2bc44279e960999b4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc9ddf94d44c8ab3dd41f212324eca29",
+    "url": "/Job-Portal-using-github-Job-Api-/index.html"
+  },
+  {
+    "revision": "38d51ca41075a3290212",
+    "url": "/Job-Portal-using-github-Job-Api-/static/css/main.909b481b.chunk.css"
+  },
+  {
+    "revision": "3a76ecbba818313232f5",
+    "url": "/Job-Portal-using-github-Job-Api-/static/js/2.74f97ca3.chunk.js"
+  },
+  {
+    "revision": "ce12de340ef4804f0d244ce30d144354",
+    "url": "/Job-Portal-using-github-Job-Api-/static/js/2.74f97ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38d51ca41075a3290212",
+    "url": "/Job-Portal-using-github-Job-Api-/static/js/main.42cecc59.chunk.js"
+  },
+  {
+    "revision": "b1d8f6a47c414b0c2468",
+    "url": "/Job-Portal-using-github-Job-Api-/static/js/runtime-main.c8b5d836.js"
+  },
+  {
+    "revision": "0feef502755ee76d1d6d396562fc139f",
+    "url": "/Job-Portal-using-github-Job-Api-/static/media/githubjobapi.0feef502.png"
+  }
+]);
